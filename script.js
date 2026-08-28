261c4f2cf33b6444762d26fccd6fe339f1c6b026
Kacper Popek <monsyer888@gmail.com>
2026-08-27 20:24:56 -0400

    Create dynamic project rendering

--- script.js
+++ script.js
@@ -15,3 +15,44 @@ window.addEventListener("scroll", function() {
     document.querySelector("header").style.backgroundColor = backgroundColor;
 
 });
+
+
+const projects=[
+
+    {   
+        name:"Expense Tracker",
+        language:"Python",
+        description: "Tracks expenses ",
+
+    },
+
+        {
+        name: "Black Jack Game",
+        language: "Python",
+        description: "Simple Black Jack game"
+    },
+
+    {
+        name: "Budget Application",
+        language: "Python, Flask",
+        description: "Website for tracking my expenses"
+    }
+
+];
+
+
+
+const projectList = document.getElementById("project-list");
+
+let i = 0;
+
+while (i < projects.length) {
+
+    const projectItem = document.createElement("li");
+
+    projectItem.textContent = projects[i].name;
+
+    projectList.appendChild(projectItem);
+
+    i++;
+}
